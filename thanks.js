6902c7aebaad0c85d53b21a1096a1d723aff460a
k0gane <50335010+k0gane@users.noreply.github.com>
2019-08-28 03:09:57 +0900

    最新版
    とりあえずここから

--- thanks.js
+++ thanks.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
         var data = new FormData;
         
         //メールを取得
-        data.append('entry.307426390', $("#googleform input[name=email]").val());
+        data.append('entry.307426390', $("#email").val());
         //名前を取得
-        data.append('entry.718895314', $("#googleform input[name=name]").val());
+        data.append('entry.718895314', $("#name").val());
         //歳を取得
-        data.append('entry.1891173114', $("#googleform input[name=age]:checked").val());
+        data.append('entry.1891173114', $("#age").val());
         //お問い合わせ内容を取得
-        data.append('entry.1011271364', $("#googleform input[name=msg]").val());
+        data.append('entry.1011271364', $("#msg").val());
 
-        console.log(data);
+
  
         $.ajax({
             url: "https://docs.google.com/forms/d/e/1FAIpQLScvmbS1iM6JR6ou9lFaiwOKhjx4HBUOJqmdfybsmJ2-mCdthw/formResponse",
             data: data,
             processData: false,
-            contenttype: false,
+            contentType: false,
             type: "POST",
             statusCode: {
                 0: function () {

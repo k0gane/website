71bf892cb83d2f0c32255e7af598ae61c4d3cf2c
k0gane <50335010+k0gane@users.noreply.github.com>
2019-08-28 01:46:43 +0900

    Add files
    とりあえずメインページとお問い合わせに関するものだけ

---
+++ form.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>お問い合わせ</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <script type="text/javascript" src="thanks.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div class="header">
+            <div class="header-logo">k0gane's page</div>
+            <div class="header-list">
+                <a href="#" class="list hanbetu">シャニマスキャラ判定機<a>
+                <a href="#" class="list browser">専用ブラウザ<a>
+            </div>
+        </div>
+        
+
+  <div class="main">
+    <div id="form" class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post">
+        <div class="address">
+          <label for="email">メールアドレス<br></label>
+          <input id="email" type="text" name="entry.307426390" placeholder="example@mail.co.jp" required/>
+        </div>
+
+        <div class="name">
+          <label for="name">名前<br></label>
+          <input id="name" type="text" name="entry.718895314" placeholder="なまえ" />
+        <div class="age">
+        <label for="age">年齢<br></label>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="18歳未満"/>18歳未満</label>
+          <br>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="18〜30歳"/>18〜30歳</label>
+          <br>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="31〜40歳"/>31〜40歳</label>
+          <br>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="41〜50歳"/>41〜50歳</label>
+          <br>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="51〜60歳"/>51〜60歳</label>
+          <br>
+          <label for="age"><input id="age" type="radio" name="entry.1891173114" value="61歳以上"/>61歳以上</label>
+          <br>
+        </div>
+        <div class="otoiawase">
+          <label for="msg">お問い合わせ内容<br></label>
+          <textarea id="msg" name="entry.1011271364" placeholder="お気軽にお問い合わせ下さい。" ></textarea>
+        </div>
+        <input id="submitBtn" type="submit" value="送信">
+      </form>
+    </div>
+
+    <div id="success" class="success">
+      <h2>送信完了しました。</h2>
+  </div>
+
+  <div id="error" class="error">
+      <h2>送信に失敗しました。ページを更新して再度更新して下さい。</h2>
+  </div>
+  
+  <div class="footer">
+                <div class="footer-logo">k0gane</div>
+                <div class="footer-list">
+                    <a href="https://www.twitter.com/23k_h" class="btn twitter">
+                        <span class="fa fa-twitter"></span>
+                        twitter:@23k_h
+                    </a>
+                    <a href="#" class="btn call">お問い合わせ</a>
+                </div>
+        </div>
+    </body>
+</html>
---
+++ thanks.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $("#googleform").submit(function (event) {
+        
+        var data = new FormData;
+        
+        //メールを取得
+        data.append('entry.307426390', $("#googleform input[name=email]").val());
+        //名前を取得
+        data.append('entry.718895314', $("#googleform input[name=name]").val());
+        //歳を取得
+        data.append('entry.1891173114', $("#googleform input[name=age]:checked").val());
+        //お問い合わせ内容を取得
+        data.append('entry.1011271364', $("#googleform input[name=msg]").val());
+
+
+ 
+        $.ajax({
+            url: "https://docs.google.com/forms/d/e/1FAIpQLScvmbS1iM6JR6ou9lFaiwOKhjx4HBUOJqmdfybsmJ2-mCdthw/formResponse",
+            data: data,
+            processData: false,
+            contenttype: false,
+            type: "POST",
+            statusCode: {
+                0: function () {
+                    //送信に成功したときの処理
+                    $("form").slideUp();
+                    $('#success').slideDown();
+                },
+                200: function () {
+                    //送信に失敗したときの処理
+                    $("form").slideUp();
+                    $('#error').slideDown();
+                }
+            }
+        });
+        event.preventDefault();
+    });
+});
